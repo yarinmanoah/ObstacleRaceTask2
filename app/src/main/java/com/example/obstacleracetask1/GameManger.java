21076a9579a2419d91c50ea966b9311fc5ede38a
yarinmanoah <127767618+yarinmanoah@users.noreply.github.com>
2024-02-11 17:12:31 +0200

    copy Task 1 to Task 2

---
+++ app/src/main/java/com/example/obstacleracetask1/GameManger.java
@@ -0,0 +1,94 @@
+package com.example.obstacleracetask1;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class GameManger {
+    public static final int MAX_LIVES = 3,COLUMNS = 3,ROWS = 5;
+    public boolean[] lifes;
+    public boolean[][] activeIos;
+    private int lives = MAX_LIVES,AndroidIndex;
+    boolean isHit, finish;
+
+    public GameManger() {
+        AndroidIndex =1;
+        activeIos =new boolean[ROWS][COLUMNS];
+        initLives();
+    }
+    public void initLives(){
+        lifes=new boolean[MAX_LIVES];
+        Arrays.fill(lifes, true);
+    }
+    public void reduceLives() {
+        lives--;
+    }
+
+    public  int getROWS() {
+        return ROWS;
+    }
+
+    public  int getCOLUMNS() {
+        return COLUMNS;
+    }
+
+    public int getRandom(){
+        Random rand = new Random();
+        return rand.nextInt(COLUMNS);
+    }
+    public boolean isActive(int row,int col) {
+        return activeIos[row][col];
+    }
+    public void updateGame() {
+        for (int i = getROWS()-1; i >=0;i--){
+            for (int j = 0; j <getCOLUMNS();j++){
+                if( isActive(i,j) && i == getROWS()-1) {
+                    activeIos[i][j] = false;
+
+                    if (j == AndroidIndex) {
+                        isHit = true;
+                        if(lives==0){
+                            lives = MAX_LIVES; //update lives when over
+                        }
+                        reduceLives();
+                        lifes[lives] = false;
+                        if(!lifes[0]) { initLives();} // Update hearts
+
+                        //if (lives == 0)
+                           // finish = true;
+
+                    }
+                }
+                else if(i != getROWS()-1){
+                    activeIos[i+1][j]= activeIos[i][j];
+
+                }
+            }
+        }
+    }
+    public void updateNewIos(){
+        int col =getRandom();
+        for(int i = 0;i<getCOLUMNS();i++){
+            activeIos[0][i]= col == i;
+        }
+    }
+    public void update(){
+        updateGame();
+        updateNewIos();
+    }
+
+    public void setHit(boolean hit) {
+        isHit = hit;
+    }
+
+    public boolean[] getLifes() {
+        return lifes;
+    }
+
+    public boolean isFinish() {
+        return finish;
+    }
+
+    public void setAndroidIndex(int androidIndex) {
+        this.AndroidIndex = androidIndex;
+    }
+}
---
+++ app/src/main/java/com/example/obstacleracetask1/MainActivity.java
@@ -0,0 +1,207 @@
+package com.example.obstacleracetask1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.bumptech.glide.Glide;
+import com.google.android.material.imageview.ShapeableImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private AppCompatImageButton game_BTN_left,game_BTN_right;
+    private ImageView[] game_IMG_player;
+    private ImageView[][] game_IMG_ios;
+    private ShapeableImageView[] game_IMG_hearts;
+    private ShapeableImageView space_IMG_background;
+    public static final int DELAY = 1000;
+    //public static final int vibrate = 500;
+    final Handler handler = new Handler();
+    private GameManger gameManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findView();
+
+        Glide
+                .with(this)
+                .load("https://www.pngall.com/wp-content/uploads/2016/07/Space-PNG-HD.png").into((space_IMG_background));
+
+        gameManager = new GameManger();
+        initViews();
+        startTimer();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopTimer();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startTimer();
+    }
+
+    private void initViews() {
+        game_BTN_left.setOnClickListener(v -> {
+            if (game_IMG_player[1].isShown()) {
+                game_IMG_player[0].setVisibility(View.VISIBLE);
+                game_IMG_player[1].setVisibility(View.INVISIBLE);
+                gameManager.setAndroidIndex(0);
+            } else if (game_IMG_player[2].isShown()) {
+                game_IMG_player[1].setVisibility(View.VISIBLE);
+                game_IMG_player[2].setVisibility(View.INVISIBLE);
+                gameManager.setAndroidIndex(1);
+            }
+        });
+        game_BTN_right.setOnClickListener(view -> {
+            if (game_IMG_player[0].isShown()) {
+                game_IMG_player[0].setVisibility(View.INVISIBLE);
+                game_IMG_player[1].setVisibility(View.VISIBLE);
+                game_IMG_player[2].setVisibility(View.INVISIBLE);
+                gameManager.setAndroidIndex(1);
+
+            } else if (game_IMG_player[1].isShown()) {
+                game_IMG_player[0].setVisibility(View.INVISIBLE);
+                game_IMG_player[1].setVisibility(View.INVISIBLE);
+                game_IMG_player[2].setVisibility(View.VISIBLE);
+                gameManager.setAndroidIndex(2);
+            }
+        });
+    }
+
+    private void findView() {
+        game_BTN_left = findViewById(R.id.game_BTN_left);
+        game_BTN_right = findViewById(R.id.game_BTN_right);
+        space_IMG_background = findViewById(R.id.space_IMG_background);
+        initIosArr();
+        initHeartArr();
+        initAndroidArr();
+    }
+
+    private void initAndroidArr() {
+        game_IMG_player = new ImageView[]{
+                findViewById(R.id.game_IMG_PlayerLeft),
+                findViewById(R.id.game_IMG_PlayerCenter),
+                findViewById(R.id.game_IMG_PlayerRight),
+        };
+    }
+
+    private void initHeartArr() {
+        game_IMG_hearts = new ShapeableImageView[]{
+                findViewById(R.id.game_IMG_heart3),
+                findViewById(R.id.game_IMG_heart2),
+                findViewById(R.id.game_IMG_heart1),
+        };
+    }
+
+    private void initIosArr() {
+        game_IMG_ios = new ImageView[][]{
+                {findViewById(R.id.game_IMG_ios1),
+                        findViewById(R.id.game_IMG_ios2),
+                        findViewById(R.id.game_IMG_ios3)},
+
+                {findViewById(R.id.game_IMG_ios4),
+                        findViewById(R.id.game_IMG_ios5),
+                        findViewById(R.id.game_IMG_ios6)},
+
+                {findViewById(R.id.game_IMG_ios7),
+                        findViewById(R.id.game_IMG_ios8),
+                        findViewById(R.id.game_IMG_ios9)},
+
+                {findViewById(R.id.game_IMG_ios10),
+                        findViewById(R.id.game_IMG_ios11),
+                        findViewById(R.id.game_IMG_ios12)},
+
+                {findViewById(R.id.game_IMG_ios13),
+                        findViewById(R.id.game_IMG_ios14),
+                        findViewById(R.id.game_IMG_ios15)}
+        };
+    }
+
+    private void refreshHearts() {
+        boolean[] lifes = gameManager.getLifes();
+            for (int i = 0; i < lifes.length; i++) {
+                if (lifes[i])
+                    game_IMG_hearts[i].setVisibility(View.VISIBLE);
+                else
+                    game_IMG_hearts[i].setVisibility(View.INVISIBLE);
+                }
+    }
+
+
+
+    private void refreshIosUI() {
+        for (int i = 0; i < gameManager.getROWS(); i++) {
+            for (int j = 0; j < gameManager.getCOLUMNS(); j++) {
+                if (gameManager.isActive(i, j))
+                    game_IMG_ios[i][j].setVisibility(View.VISIBLE);
+                else game_IMG_ios[i][j].setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    private void refreshUI() {
+        gameManager.update();
+        //if (gameManager.isFinish()) {
+        //    vibrate();
+        //    refreshHearts();
+        //    refreshIosUI();
+        //    Toast
+        //            .makeText(this, "Game Over", Toast.LENGTH_SHORT)
+        //            .show();
+        //    stopTimer();
+        //    finish();
+        //} else {
+            if (gameManager.isHit) {
+                refreshHearts();
+                Toast
+                        .makeText(this, "HIT", Toast.LENGTH_SHORT)
+                        .show();
+                vibrate();
+                gameManager.setHit(false);
+            }
+       // }
+        refreshIosUI();
+    }
+
+    Runnable runnable = new Runnable() {
+        public void run() {
+            handler.postDelayed(this, DELAY);
+            refreshUI();
+        }
+    };
+
+    private void startTimer() {
+        handler.postDelayed(runnable, DELAY);
+    }
+
+    private void stopTimer() {
+        handler.removeCallbacks(runnable);
+    }
+
+    private void vibrate() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v.vibrate(500);
+        }
+    }
+}
+
+
